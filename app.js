de2cd20558ecdb98ff7ca5dc28655a00dd643757
DoQuocBinh <binhdq@fpt.edu.vn>
2020-07-08 17:09:22 +0700

    aaa

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log(2+1)
+i = 2;
+console.log(i)
+for(i=1;i<10;i++){
+    console.log(i);
+}
+name = 'Viet nam muon nam1111sdsds';
+console.log('vi tri thu 2' + name.charAt(2));
+//console.log('Name co ' + name.length);
+console.log(`name co ${name.length} ky tu`);
+var str = "Hello world, welcome to the universe.";
+var n = str.endsWith("universe.");
+console.log(n);
