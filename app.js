d4786967a57cc1de5339e67a920461883d1d0894
DoQuocBinh <binhdq@fpt.edu.vn>
2020-07-08 18:20:04 +0700

    aaa

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ console.log(i)
 for(i=1;i<10;i++){
     console.log(i);
 }
-name = 'Viet nam muon nam1111sdsds';
+name = '111Viet nam muon nam1111sdsds';
 console.log('vi tri thu 2' + name.charAt(2));
 //console.log('Name co ' + name.length);
 console.log(`name co ${name.length} ky tu`);
